55b28b5af8119c35bcde609e36f86634eaa09d06
yewon0804 <ehdnjs0804@gmail.com>
2023-06-26 08:57:03 +0000

    로그인 시, 안내 메시지에 닉네임이 null로 표시되던 현상 수정

--- auth/login.php
+++ auth/login.php
@@ -4,15 +4,16 @@ include_once '../dbconfig.php';
 $userid = $_POST["userid"];
 $email = $_POST["email"];
 $userpw = $_POST["userpw"];
+$nickname = $_POST["nickname"];
 
 
-$statement = mysqli_prepare($conn, "SELECT * FROM user WHERE userid = ? AND userpw = sha2(?,256)");
+$statement = mysqli_prepare($conn, "SELECT userid, email, nickname FROM user WHERE userid = ? AND userpw = sha2(?,256)");
 mysqli_stmt_bind_param($statement, "ss", $userid, $userpw);
 mysqli_stmt_execute($statement);
 
 
 mysqli_stmt_store_result($statement);
-mysqli_stmt_bind_result($statement, $userid, $email, $userpw, $nickname);
+mysqli_stmt_bind_result($statement, $userid, $email, $nickname);
 
 $response = array();
 $response["success"] = false;
@@ -21,7 +22,6 @@ while(mysqli_stmt_fetch($statement)) {
 	$response["success"] = true;
 	$response["userid"] = $userid;
 	$response["email"] = $email;
-	$response["userpw"] = $userpw;
 	$response["nickname"] = $nickname;
 }
 
