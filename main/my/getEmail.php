5bf1a7f4d8712899f43797783c1a383d01b94fd9
yewon0804 <ehdnjs0804@gmail.com>
2023-06-29 18:08:15 +0000

    Add getEmail.php

---
+++ main/my/getEmail.php
@@ -0,0 +1,22 @@
+<?php
+include_once '../../dbconfig.php';
+
+$userid = $_POST["userid"];
+
+$statement = mysqli_prepare($conn, "SELECT email FROM user WHERE userid = ?");
+mysqli_stmt_bind_param($statement, "s", $userid);
+mysqli_stmt_execute($statement);
+
+mysqli_stmt_store_result($statement);
+mysqli_stmt_bind_result($statement, $email);
+
+$response = array();
+$response["success"] = false;
+
+while(mysqli_stmt_fetch($statement)){
+	$response["success"]=true;
+	$response["email"]=$email;
+}
+
+echo json_encode($response);
+?>  
