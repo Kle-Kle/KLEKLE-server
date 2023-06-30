e8b14f3be32e330a322749e82db229b0d4184ff5
yewon0804 <ehdnjs0804@gmail.com>
2023-06-30 15:54:06 +0000

    [BUG FIX]닉네임 변경 시, 이미 사용 중인 닉네임으로는 변경할 수 없음

--- main/my/updateNickname.php
+++ main/my/updateNickname.php
@@ -6,10 +6,16 @@ $nickname = $_POST["nickname"];
 
 $statement = mysqli_prepare($conn, "UPDATE user SET nickname=? WHERE nickname=?");
 mysqli_stmt_bind_param($statement, "ss", $newNickname, $nickname);
-mysqli_stmt_execute($statement);    
+mysqli_stmt_execute($statement);
 
 $response = array();
-$response["success"] = true;
+// mysqli_stmt_execute(): 성공하면 true, 실패하면 false를 반환
+if (mysqli_stmt_execute($statement)) {
+    $response["success"] = true;
+}
+else {
+    $response["success"] = false;
+}
 
 echo json_encode($response);
 ?>  
