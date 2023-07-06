82e3eafd3cda97e7b8b927322966735206ff6f56
yewon0804 <ehdnjs0804@gmail.com>
2023-07-06 11:14:30 +0000

    Update: 회원가입 시, 기본 프로필 사진이 DB에 저장되도록 업데이트

--- auth/register.php
+++ auth/register.php
@@ -1,5 +1,6 @@
 <?php
 include_once '../dbconfig.php';
+include_once './initProfile.php';
 
 $userid = $_POST["userid"];
 $email = $_POST["email"];
@@ -10,10 +11,10 @@ $height = $_POST["height"];
 $weight = $_POST["weight"];
 $reach = $_POST["reach"];
 
-$statement = mysqli_prepare($conn, "INSERT INTO user VALUES (?,?,sha2(?,256),?,null)");
+$statement = mysqli_prepare($conn, "INSERT INTO user VALUES (?,?,sha2(?,256),?,?)");
 $statement2 = mysqli_prepare($conn, "INSERT INTO bodyinfo VALUES (?,?,?,?)");
 
-mysqli_stmt_bind_param($statement, "ssss", $userid, $email, $userpw, $nickname);
+mysqli_stmt_bind_param($statement, "sssss", $userid, $email, $userpw, $nickname, $profile);
 mysqli_stmt_execute($statement);
 
 mysqli_stmt_bind_param($statement2, "ssss", $userid, $height, $weight, $reach);
