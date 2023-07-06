e68e7470201e692545407b22a3999ea854d4828e
yewon0804 <ehdnjs0804@gmail.com>
2023-07-06 08:35:06 +0000

    [FEAT]사용자 프로필사진 변경: update POST 요청

---
+++ main/my/updateProfile.php
@@ -0,0 +1,20 @@
+<?php
+include_once '../../dbconfig.php';
+
+$userid = $_POST["userid"]; // userid 대신 nickname을 써봤는데, 문제 예상될 시 수정하도록
+$profile = $_POST["profile"];
+
+$statement = mysqli_prepare($conn, "UPDATE user SET profile=? WHERE userid=?");
+mysqli_stmt_bind_param($statement, "ss", $profile, $userid);
+mysqli_stmt_execute($statement);
+
+$response = array();
+if (mysqli_stmt_execute($statement)) {
+    $response["success"] = true;
+}
+else {
+    $response["success"] = false;
+}
+
+echo json_encode($response);
+?>
