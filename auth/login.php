08e9acbd20537e598b19ea05199a4523e6aed1ca
yewon0804 <ehdnjs0804@gmail.com>
2023-07-06 11:15:07 +0000

    Update: 로그인 시 profile 데이터도 반환하도록 처리

--- auth/login.php
+++ auth/login.php
@@ -2,27 +2,27 @@
 include_once '../dbconfig.php';
 
 $userid = $_POST["userid"];
-$email = $_POST["email"];
 $userpw = $_POST["userpw"];
-$nickname = $_POST["nickname"];
 
 
-$statement = mysqli_prepare($conn, "SELECT userid, email, nickname FROM user WHERE userid = ? AND userpw = sha2(?,256)");
+$statement = mysqli_prepare($conn, "SELECT userid, email, nickname, profile FROM user WHERE userid = ? AND userpw = sha2(?,256)");
 mysqli_stmt_bind_param($statement, "ss", $userid, $userpw);
 mysqli_stmt_execute($statement);
 
 
 mysqli_stmt_store_result($statement);
-mysqli_stmt_bind_result($statement, $userid, $email, $nickname);
+mysqli_stmt_bind_result($statement, $userid, $email, $nickname, $profile);
 
 $response = array();
-$response["success"] = false;
-
-while(mysqli_stmt_fetch($statement)) {
+if(mysqli_stmt_fetch($statement)) {
 	$response["success"] = true;
 	$response["userid"] = $userid;
 	$response["email"] = $email;
 	$response["nickname"] = $nickname;
+	$response["profile"] = $profile;
+}
+else {
+	$response["success"] = false;
 }
 
 echo json_encode($response);
