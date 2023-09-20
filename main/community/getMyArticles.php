5bda9b412923785f586417b05be723ddc173fb8c
yewon0804 <ehdnjs0804@gmail.com>
2023-09-20 05:08:21 +0000

    feat: get my articles API: 특정 사용자의 articles -> 개인 프로필 페이지에 이용

---
+++ main/community/getMyArticles.php
@@ -0,0 +1,32 @@
+<?php
+include_once '../../dbconfig.php';
+
+$userid = $_POST["userid"];
+
+$statement = mysqli_prepare($conn, "SELECT * FROM view_article_detail WHERE userid = ? ");
+mysqli_stmt_bind_param($statement, "s", $userid);
+mysqli_stmt_execute($statement);
+mysqli_stmt_store_result($statement);
+mysqli_stmt_bind_result($user_nickname, $user_id, $user_profile, $article_no, $article_content, $comment_count, $article_image);
+
+$response = array();
+$response["success"] = false;
+$response["result"] = array();
+
+$count = 0;
+
+while(mysqli_stmt_fetch($statement)) {
+	$response["success"] = true;
+	$response["result"][$count]["user_nickname"]=$user_nickname;
+	$response["result"][$count]["user_id"]=$user_id;
+	$response["result"][$count]["user_profile"]=$user_profile;
+	$response["result"][$count]["article_no"]=$article_no;
+	$response["result"][$count]["article_content"]=$article_content;
+	$response["result"][$count]["comment_count"]=$comment_count;
+	$response["result"][$count]["article_image"]=$article_image;
+
+	$count++;
+}
+
+echo json_encode($response);
+?>
