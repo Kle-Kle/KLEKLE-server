a894083143a3d3938ad87b21eab1a0318dd5fa18
yewon0804 <ehdnjs0804@gmail.com>
2023-09-20 05:07:36 +0000

    feat: get comments: 특정 article에 대한 댓글들: 댓글 작성자 정보, 댓글 정보

---
+++ main/community/getComments.php
@@ -0,0 +1,33 @@
+<?php
+include_once '../../dbconfig.php';
+
+$article_no = $_POST["articleNo"];
+
+$statement = mysqli_prepare($conn, "select * from view_comment_with_user where articleNo=?");
+mysqli_stmt_bind_param($statement, "i", $article_no);
+mysqli_stmt_execute($statement);
+mysqli_stmt_store_result($statement);
+mysqli_stmt_bind_result($user_nickname, $userid, $user_profile, $comment_no, $comment_content, $commented, $userid, $articleNo);
+
+$response = array();
+$response["success"] = false;
+$response["result"] = array();
+
+$count = 0;
+
+while(mysqli_stmt_fetch($statement)) {
+	$response["success"] = true;
+	$response["result"][$count]["user_nickname"]=$user_nickname;
+	$response["result"][$count]["userid"]=$userid;
+	$response["result"][$count]["user_profile"]=$user_profile;
+	$response["result"][$count]["comment_no"]=$comment_no;
+	$response["result"][$count]["content"]=$content;
+	$response["result"][$count]["commented"]=$commented;
+	$response["result"][$count]["userid"]=$userid;
+	$response["result"][$count]["article_no"]=$articleNo;
+
+	$count++;
+}
+
+echo json_encode($response);
+?>
