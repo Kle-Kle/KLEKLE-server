e7c394a1420e9c57a674636c8b34d80ff24bc4e6
yewon0804 <ehdnjs0804@gmail.com>
2023-09-29 15:29:21 +0000

    UPDATE: user table에 '상태메시지' 추가에 따른 변동 사항 적용

--- auth/login.php
+++ auth/login.php
@@ -5,13 +5,13 @@ $userid = $_POST["userid"];
 $userpw = $_POST["userpw"];
 
 
-$statement = mysqli_prepare($conn, "SELECT userid, email, nickname, profile FROM user WHERE userid = ? AND userpw = sha2(?,256)");
+$statement = mysqli_prepare($conn, "SELECT userid, email, nickname, profile, message FROM user WHERE userid = ? AND userpw = sha2(?,256)");
 mysqli_stmt_bind_param($statement, "ss", $userid, $userpw);
 mysqli_stmt_execute($statement);
 
 
 mysqli_stmt_store_result($statement);
-mysqli_stmt_bind_result($statement, $userid, $email, $nickname, $profile);
+mysqli_stmt_bind_result($statement, $userid, $email, $nickname, $profile, $message);
 
 $response = array();
 if(mysqli_stmt_fetch($statement)) {
@@ -20,6 +20,7 @@ if(mysqli_stmt_fetch($statement)) {
 	$response["email"] = $email;
 	$response["nickname"] = $nickname;
 	$response["profile"] = $profile;
+	$response["message"] = $message;
 }
 else {
 	$response["success"] = false;
