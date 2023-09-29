2ba47c5fb8fb1b9d05503370281d4fb971b8410f
yewon0804 <ehdnjs0804@gmail.com>
2023-09-29 15:29:41 +0000

    FEAT: update 상태메시지 기능 구현

---
+++ main/my/updateStatusMessage.php
@@ -0,0 +1,21 @@
+<?php
+include_once '../../dbconfig.php';
+
+$newStatusMessage = $_POST["newStatusMessage"];
+$userid = $_POST["userid"];
+
+$statement = mysqli_prepare($conn, "UPDATE user SET message=? WHERE userid=?");
+mysqli_stmt_bind_param($statement, "ss", $newStatusMessage, $userid);
+mysqli_stmt_execute($statement);
+
+$response = array();
+// mysqli_stmt_execute(): 성공하면 true, 실패하면 false를 반환
+if (mysqli_stmt_execute($statement)) {
+    $response["success"] = true;
+}
+else {
+    $response["success"] = false;
+}
+
+echo json_encode($response);
+?>  
