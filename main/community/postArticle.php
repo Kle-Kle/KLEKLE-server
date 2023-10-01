0214eb71bc1b117b88d7980a5e18614b66cf2d42
yewon0804 <ehdnjs0804@gmail.com>
2023-10-01 03:03:38 +0000

    BUG FIX: post article 정상 동작

--- main/community/postArticle.php
+++ main/community/postArticle.php
@@ -1,5 +1,5 @@
 <?php
-include_once '../dbconfig.php';
+include_once '../../dbconfig.php';
 include_once './initProfile.php';
 
 $content = $_POST["content"];
@@ -8,10 +8,15 @@ $image = $_POST["image"];
 
 $statement = mysqli_prepare($conn, "INSERT INTO article VALUES (null, null, NOW(), ?, ?, ?)"); // 본문 내용, userid, image
 mysqli_stmt_bind_param($statement, "sss", $content, $userid, $image);
-mysqli_stmt_execute($statement);
 
 $response = array();
-$response["success"] = true;
+// mysqli_stmt_execute(): 성공하면 true, 실패하면 false를 반환
+if (mysqli_stmt_execute($statement)) {
+    $response["success"] = true;
+}
+else {
+    $response["success"] = false;
+}
 
 echo json_encode($response);
 ?>
