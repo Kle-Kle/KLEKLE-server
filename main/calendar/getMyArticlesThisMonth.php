6ac45c29dc3251f985d0c4d8f5c9cff10228c00c
yewon0804 <ehdnjs0804@gmail.com>
2023-10-03 13:00:27 +0000

    FEAT: get my articles this month 특정 년도+월에 쓰인 모든 article들의 published 뱉어줌

---
+++ main/calendar/getMyArticlesThisMonth.php
@@ -0,0 +1,29 @@
+<?php
+include_once '../../dbconfig.php';
+
+$currentDate = $_POST["currentDate"];
+$userid = $_POST["userid"];
+
+$statement = mysqli_prepare($conn, "SELECT userid, published FROM view_article_preview WHERE published LIKE ? HAVING userid=?");
+mysqli_stmt_bind_param($statement, "ss", $currentDate, $userid);
+mysqli_stmt_execute($statement);
+mysqli_stmt_store_result($statement);
+mysqli_stmt_bind_result($statement, $userId, $published);
+
+$response = array();
+$response["success"] = false;
+$response["result"] = array();
+
+$count = 0;
+
+while(mysqli_stmt_fetch($statement)) {
+	$response["success"] = true;
+	$response["result"][$count]["userId"]=$userId;
+	$response["result"][$count]["published"]=$published;
+
+	$count++;
+}
+
+header('Content-type: text/javascript');
+echo json_encode($response, JSON_PRETTY_PRINT);
+?>
